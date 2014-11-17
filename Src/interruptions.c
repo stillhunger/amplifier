ab4d29bc31ac97c0f945e09b96e6a58950f69aaf
Vivien Henry <vivien.henry@outlook.fr>
2014-11-17 20:00:23 +0100

    Following the changes from lukhe Engine

---
+++ Src/interruptions.c
@@ -0,0 +1,22 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx.h"
+
+#include "interruptions.h"
+
+/**
+* @brief This function handles System tick timer.
+*/
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+
